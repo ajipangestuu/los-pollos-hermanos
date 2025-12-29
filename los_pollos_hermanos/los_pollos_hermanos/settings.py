c1d1985248379ad031186bb89a0ba8406715a75b
Adjie <aji@gmail.com>
2025-12-29 14:54:09 +0700

    Fix error Internal Server Error

--- los_pollos_hermanos/los_pollos_hermanos/settings.py
+++ los_pollos_hermanos/los_pollos_hermanos/settings.py
@@ -130,7 +130,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "9eee44001@smtp-brevo.com"
 EMAIL_HOST_PASSWORD = os.environ.get("BREVO_SMTP_KEY")
 
-DEFAULT_FROM_EMAIL = "Los Pollos <noreply@lospollos.com>"
+DEFAULT_FROM_EMAIL = "ajipangesstu165@gmail.com"
 EMAIL_TIMEOUT = 5
 
 
